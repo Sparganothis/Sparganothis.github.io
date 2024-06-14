a528ea711cc37a716eb63e942ac145a250bdd8f7
Github Cosmin <cosmin.posirca@yahoo.com>
2024-06-14 23:26:53 +0300

    merge din nou

--- webapp/src/game/tet.rs
+++ webapp/src/game/tet.rs
@@ -616,26 +616,6 @@ impl GameState {
         }
 
         let mut new_current_pcs: CurrentPcsInfo = current_pcs;
-        if (new_current_pcs.tet.name()=="I"){
-            if new_current_pcs.pos.1==-2 {
-                new_current_pcs.pos.1+=1;
-            }
-            else {
-                if new_current_pcs.pos.1==7 {
-                new_current_pcs.pos.1-=1;
-            }
-            }
-        }
-        if new_current_pcs.tet.name()!="O"{
-            if new_current_pcs.pos.1==-1 {
-                new_current_pcs.pos.1+=1;
-            }
-            else {
-                if new_current_pcs.pos.1==8 {
-                new_current_pcs.pos.1-=1;
-            }
-            }
-        }
         new_current_pcs.rs = new_current_pcs.rs.rotate(RotDirection::Left);
 
         self.main_board.spawn_piece(&new_current_pcs)?;
@@ -651,26 +631,6 @@ impl GameState {
         }
 
         let mut new_current_pcs = current_pcs;
-        if (new_current_pcs.tet.name()=="I"){
-            if new_current_pcs.pos.1==-2 {
-                new_current_pcs.pos.1+=1;
-            }
-            else {
-                if new_current_pcs.pos.1==7 {
-                new_current_pcs.pos.1-=1;
-            }
-            }
-        }
-        if (new_current_pcs.tet.name()!="O"){
-            if new_current_pcs.pos.1==-1 {
-                new_current_pcs.pos.1+=1;
-            }
-            else {
-                if new_current_pcs.pos.1==8 {
-                new_current_pcs.pos.1-=1;
-            }
-            }
-        }
         new_current_pcs.rs = new_current_pcs.rs.rotate(RotDirection::Right);
 
         self.main_board.spawn_piece(&new_current_pcs)?;
