9d8af08a3f8c605131d55224963dffbc876da56b
Github Cosmin <cosmin.posirca@yahoo.com>
2024-06-22 04:16:44 +0300

    l si j

--- client/src/comp/multiselect_repeat.rs
+++ client/src/comp/multiselect_repeat.rs
@@ -11,12 +11,46 @@ pub fn MultiSelectSmecher(
     #[prop(into)] selected: Signal<Vec<Tet>>,
     #[prop(into)] set_selected: Callback<Vec<Tet>>,)-> impl IntoView{
 
-        let analfabet = Tet::all().iter().map(|x|{
-            format!("{:?}",*x)
-        }).collect::<Vec<_>>().as_slice().join("") ;
-        let text = || -> String{"Penis".to_string()};
-        let set_text = |t: String|{
-            log::info!("{}",t);
+        // let analfabet = Tet::all().iter().map(|x|{
+        //     format!("{:?}",*x)
+        // }).collect::<Vec<_>>().as_slice().join("") ;
+        let text = move || -> String{
+            selected.get().into_iter().map(|c| {
+                format!("{:?}", c)
+            }).collect::<Vec<_>>().as_slice().join("")
+        };
+        let set_text = move |t: String|{
+            let vt : Vec<Option<Tet>> = t.chars().map(|c| {
+                match c {
+                    'i'|'I' => {
+                        Some(Tet::I)
+                    },
+                    'j'|'J' => {
+                        Some(Tet::J)
+                    },
+                    's'|'S' => {
+                        Some(Tet::S)
+                    },
+                    'z'|'Z' => {
+                        Some(Tet::Z)
+                    },
+                    'o'|'O' => {
+                        Some(Tet::O)
+                    },
+                    't'|'T' => {
+                        Some(Tet::T)
+                    },
+                    'L'|'l' => {
+                        Some(Tet::L)
+                    },
+                    
+                    _=>None,
+
+                }
+
+            }).collect::<Vec<_>>();
+            let vt = vt.iter().filter(|c| c.is_some()).map(|c| c.unwrap()).collect::<Vec<_>>();
+            set_selected.call(vt);
         };
         let set_text = Callback::<String>::new(set_text);
     view!{
