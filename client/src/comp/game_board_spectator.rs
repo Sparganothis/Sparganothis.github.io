bbc74b399de36577d21408f14548970df459630b
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2024-06-23 13:42:11 +0300

    fix ddos

--- client/src/comp/game_board_spectator.rs
+++ client/src/comp/game_board_spectator.rs
@@ -13,43 +13,36 @@ pub fn SpectatorGameBoard(game_id: GameId) -> impl IntoView {
     let state = create_rw_signal(GameState::new(&seed, 0));
     let api : WebsocketAPI = expect_context();
     let game_id = game_id.clone();
+    let api2 = api.clone();
+    let game_id2 = game_id.clone();
     api.subscribe_to_game(&game_id, Callback::<_>::new(move |_update: Vec<(GameSegmentId, GameReplaySegment)>| {
-        for (key, _value) in _update.iter() {
-            log::info!("got surpriuzxe segment: {}",key.segment_id);
-            match _value {
-                GameReplaySegment::Init(init) => {
-                    state.update(|state_val| {
-                        *state_val = GameState::new(&init.init_seed, init.start_time)
-                    });
-                }
-                GameReplaySegment::Update(slice) => {
-                    state.update(|state_val| {
-                        if let Err(e) = state_val.accept_replay_slice(&slice) {
-                            log::warn!("error in accept_replay_slice() : {:?}", e);
-                        }
-                    });
-                }
-                GameReplaySegment::GameOver => {
-                    log::info!("subscribe game over!");
+        state.update(|state_val| {
+            for (key, _value) in _update.iter() {
+                log::info!("got surpriuzxe segment: {}",key.segment_id);
+                match _value {
+                    GameReplaySegment::Init(init) => {
+                            *state_val = GameState::new(&init.init_seed, init.start_time)
+                    }
+                    GameReplaySegment::Update(slice) => {
+                            if let Err(e) = state_val.accept_replay_slice(&slice) {
+                                log::warn!("error in accept_replay_slice() : {:?}", e);
+                            }
+                    }
+                    GameReplaySegment::GameOver => {
+                        log::info!("subscribe game over!");
+                        let api2 = api2.clone();
+                        queue_microtask(move || {
+                            api2.stop_subscribe_to_game(&game_id2);
+                        })
+                    }
                 }
             }
-        }
+        });
     }));
-
-    // let memo_state = create_memo(move |_| {
-    //     let segment = get_segment();
-    //     match segment {
-
-    //         None => {
-    //             log::info!("websocket message is none!~");
-    //             false
-    //         }
-    //     }
-    // });
 
     let on_reset: Callback<()> = Callback::<()>::new(move |_| {});
     view! {
-        <h1>"soectate demo"</h1>
+        <h1>"spectating game"</h1>
         <GameBoard on_reset_game=on_reset game_state=state/>
     }
 }
