6b308e0e848e1b772da3d81eaf857db7f44f380a
Github Cosmin <cosmin.posirca@yahoo.com>
2024-07-01 17:29:03 +0300

    cargo fmt Mon, Jul  1, 2024  5:29:03 PM

--- client/src/page/settings/sound.rs
+++ client/src/page/settings/sound.rs
@@ -18,16 +18,32 @@ pub fn SoundSettingsTab(user_profile: user::UserProfile, guest_id: GuestInfo) ->
         <table>
 
             // DISBLAE ALL SOUNDS
-            <ToggleSettingRow signal= user_setting_signal.all_sound_enabled title="Sound".to_string() on_label="ON".to_string() off_label="OFF".to_string()/>
+            <ToggleSettingRow
+                signal=user_setting_signal.all_sound_enabled
+                title="Sound".to_string()
+                on_label="ON".to_string()
+                off_label="OFF".to_string()
+            />
             // VOLUMME ALL SOUNDS
             <Show when=move || user_setting_signal.all_sound_enabled.get()>
-                <SliderSettingRow signal=user_setting_signal.sound_all_sounds_volume title="Volume".to_string()/>
+                <SliderSettingRow
+                    signal=user_setting_signal.sound_all_sounds_volume
+                    title="Volume".to_string()
+                />
                 // DISABLE_MENU_MUSIC
-                <ToggleSettingRow signal= user_setting_signal.sound_menu_music_enabled title="Menu Music".to_string() on_label="ON".to_string() off_label="OFF".to_string()/>
+                <ToggleSettingRow
+                    signal=user_setting_signal.sound_menu_music_enabled
+                    title="Menu Music".to_string()
+                    on_label="ON".to_string()
+                    off_label="OFF".to_string()
+                />
                 // MMENU MMUSIC VOLUME
                 <Show when=move || user_setting_signal.sound_menu_music_enabled.get()>
-                    <SliderSettingRow signal=user_setting_signal.sound_menu_music_volume title="Music Volume".to_string()/>
-                </Show>        
+                    <SliderSettingRow
+                        signal=user_setting_signal.sound_menu_music_volume
+                        title="Music Volume".to_string()
+                    />
+                </Show>
             </Show>
         </table>
     }
@@ -37,36 +53,24 @@ pub fn SoundSettingsTab(user_profile: user::UserProfile, guest_id: GuestInfo) ->
 #[component]
 pub fn SliderSettingRow(signal: RwSignal<f64>, title: String)-> impl IntoView{
 
-    view!{
-
+    view! {
         <tr>
             <td style="width:20vmin;">
                 <Slider
                     min=1.0
                     max=100.0
                     step=1.0
-                    value=signal
-                        .read_only()
-                    set_value=signal
-                        .write_only()
+                    value=signal.read_only()
+                    set_value=signal.write_only()
                     value_display=move |v| format!("{v:.0}")
                 />
             </td>
             <td>
                 <h3>{title}</h3>
             </td>
-            <td>
-                {move || {
-                    format!(
-                        "{}",
-                        signal.get(),
-                    )
-                }}
-
+            <td>{move || { format!("{}", signal.get()) }}
             </td>
         </tr>
-
-
     }
 
 
@@ -79,26 +83,19 @@ pub fn SliderSettingRow(signal: RwSignal<f64>, title: String)-> impl IntoView{
 pub fn ToggleSettingRow(signal: RwSignal<bool>, title: String, on_label: String, off_label: String)-> impl IntoView{
 
 
-    view!{
+    view! {
         <tr>
             <td style="width:20vmin;">
-                <Toggle
-                    state=signal.read_only()
-                    set_state=signal.write_only()
-                />
+                <Toggle state=signal.read_only() set_state=signal.write_only()/>
             </td>
             <td style="width:30vmin;">
                 <h3>{title}</h3>
             </td>
             <td style="width:20vmin;">
                 <h3>
-                    {
-                        move || {
-                        (if signal.get() {
-                        on_label.clone()
-                        } else {
-                        off_label.clone()
-                        })
+
+                    {move || {
+                        (if signal.get() { on_label.clone() } else { off_label.clone() })
                     }}
 
                 </h3>
