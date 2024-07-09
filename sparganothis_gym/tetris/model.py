165614bba770b6cdf9c2bf2de6c00318a4b49ec0
Oodapow <petre.v.alexandru@gmail.com>
2024-07-09 21:33:10 +0300

    start memory add

--- sparganothis_gym/tetris/model.py
+++ sparganothis_gym/tetris/model.py
@@ -1,5 +1,4 @@
 import random
-from collections import deque, namedtuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,23 +12,6 @@ device = torch.device(
     if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available() else "cpu"
 )
-
-Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
 
 class DQN(nn.Module):
 
--- sparganothis_gym/tetris/reward.py
+++ sparganothis_gym/tetris/reward.py
@@ -1,21 +1,29 @@
 def build_score_reward(alpha):
-    def score_reward(current_state, next_state):
+    def score_reward(current_state, next_state, history):
         return (next_state.score - current_state.score) * alpha
     return score_reward
 
 def build_end_reward(value):
-    def end_reward(current_state, next_state):
+    def end_reward(current_state, next_state, history):
         if next_state.game_over:
             return value
         return 0
     return end_reward
 
+def build_history_reward(value):
+    def history_reward(current_state, next_state, history):
+        return 
+    return history_reward
+
+def build_per_state_reward():
+    def per_state_reward(current_state, next_state, history):
+        return next_state.score + 500 * next_state.total_garbage_sent + 250 * next_state.total_lines - 5 * next_state.total_move_count
+    return per_state_reward
+
 def merge_rewards(reward_fns):
-    def reward_fn(current_state, next_state):
+    def reward_fn(current_state, next_state, history):
         reward = 0
         for fn in reward_fns:
-            reward += fn(current_state, next_state)
+            reward += fn(current_state, next_state, history)
         return reward
     return reward_fn
-
-default_reward = build_end_reward(-100)
